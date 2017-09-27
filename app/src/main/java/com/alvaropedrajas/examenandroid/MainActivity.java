d19b2b75884e72596346ad2590d5e1e8993fbd61
AlvaroEstrada <alvaroestrada85@gmail.com>
2017-09-28 01:36:13 +0200

    App complete with exceptions

--- app/src/main/java/com/alvaropedrajas/examenandroid/MainActivity.java
+++ app/src/main/java/com/alvaropedrajas/examenandroid/MainActivity.java
@@ -57,7 +57,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }*/
 
-
     @Override
     public void onClick(View v) {
         listaContactos = Utils.readFile(activity);
